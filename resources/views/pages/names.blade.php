c3d60601fb4a044e6b093abec17b33279cd50131
Dodii2022 <ivanczilevi@gmail.com>
2025-05-14 09:41:14 +0200

    first commit

---
+++ resources/views/pages/names.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', '| Nevek')
+
+@section('content')
+    <div class="container">
+
+        <ul>
+@foreach($names as $name)
+    <li @if($name == 'Traza') style="font-weight:bold;" @endif>
+        @if($loop->last) Utolsó: @endif
+        {{ $name }}
+    </li>
+@endforeach
+</ul>
+
+    </div>
+@endsection
